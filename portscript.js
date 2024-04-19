51bb0946770a715ea53a9619a5b3b53823cbaea7
suryabhai <suryaprakashdas921@gmail.com>
2024-04-19 12:35:55 +0530

    Initial commit

---
+++ portscript.js
@@ -0,0 +1,26 @@
+
+  
+  const navbarLinks = document.querySelectorAll('.navbar a');
+
+  navbarLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault(); // Prevent default jump
+
+      const targetSection = document.querySelector(link.getAttribute('href'));
+
+      // Optional parameters for smoother scrolling:
+      targetSection.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start', // Start section at top of viewport
+        inline: 'center' // Center section horizontally
+      });
+    });
+  });
+
+  document.getElementById('contactLink').addEventListener('mouseover', function() {
+    document.getElementById('popup').style.display = 'block';
+});
+
+document.getElementById('contactLink').addEventListener('mouseout', function() {
+    document.getElementById('popup').style.display = 'none';
+});
